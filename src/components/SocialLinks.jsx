7c13f9c29dc9a7185bca9e55cdf0eba37a1732dc
MARTINCHUNGA <Bsc-77-16@cc.ac.mw>
2023-08-29 14:49:30 +0300

    updated resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ function SocialLinks() {
                  Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href: '/Martin Chunga Resume.pdf',
+            href: '/Martin Chunga.pdf',
             style:'rounded-br-md',
             download: true     
         },
